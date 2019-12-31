2eabc89d37e54ca32953c195f16fc12390881b94
Cihan Çelen <cihan.celen37@hotmail.com>
2019-12-31 14:14:06 +0300

    rk select dosyaları oluşturuldu.

---
+++ projects/roksit-lib/src/lib/modules/rk-select/rk-select-container/rk-select-container.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'rk-select',
+    templateUrl: 'rk-select.component.html',
+})
+
+export class RkSelectComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() { }
+
+}
---
+++ projects/roksit-lib/src/lib/modules/rk-select/rk-select-option/rk-select-option.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'rk-option',
+    templateUrl: 'rk-select-option.component.html'
+})
+
+export class RkSelectOptionComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() { }
+
+}
