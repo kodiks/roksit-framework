d9e14cac6442b62b35021ae265ad4c1fd421d347
Cihan Çelen <cihan.celen37@hotmail.com>
2019-12-31 09:50:12 +0300

    checkbox eklendi

---
+++ projects/roksit-lib/src/lib/modules/rk-badge/rk-badge.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'rk-badge',
+    templateUrl: 'rk-badge.component.html'
+})
+
+export class RkBadgeComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() { }
+
+}
