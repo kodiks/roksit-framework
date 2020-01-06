5bf493d128fcdd6ebd03def9cc2bc12720bd4ec6
Cihan Çelen <cihan.celen37@hotmail.com>
2020-01-07 01:22:56 +0300

    rk-select ngmodel'e baglandi.

--- projects/roksit-lib/src/lib/modules/rk-select/rk-select.component.ts
+++ projects/roksit-lib/src/lib/modules/rk-select/rk-select.component.ts
@@ -1,17 +1,33 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, Self } from '@angular/core';
+import * as $ from 'jquery';
+import { NgModel, NG_VALUE_ACCESSOR, NgControl, ControlValueAccessor } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'rk-select',
     templateUrl: 'rk-select.component.html',
-    styleUrls: ['rk-select.component.scss']
+    styleUrls: ['rk-select.component.scss'],
+    // providers: [{
+    //     provide: NG_VALUE_ACCESSOR,
+    //     useExisting: RkSelectComponent,
+    //     multi: true,
+    // }],
 })
-export class RkSelectComponent implements OnInit {
+export class RkSelectComponent implements OnInit, ControlValueAccessor {
 
-    constructor() { }
+    constructor(
+        @Self() public controlDir: NgControl
+    ) {
+        controlDir.valueAccessor = this;
+    }
 
     @Input() options: RkSelectModel[];
 
+    @Input() disabled: boolean;
+
     @Input() chooseText = 'Lütfen seçiniz';
+
+    @Input() icon: string;
 
     selectedIndex: number;
 
@@ -19,39 +35,57 @@ export class RkSelectComponent implements OnInit {
 
     itemsContainerHidden = true;
 
-    ngOnInit() { }
+    @ViewChild(NgModel, { static: false }) model: NgModel;
+
+    private onChange: (value: string) => void;
+
+    private onTouched: (value: string) => void;
+
+    private valueChanges: Subscription;
+
+    ngOnInit() {
+        console.log(this.model);
+    }
 
     public selectOption(option: RkSelectModel, index: number) {
-        this.selectedOption = option;
+        this.writeValue(option);
+
         this.selectedIndex = index;
 
         this.setItemsContainerHidden(false);
     }
 
     setItemsContainerHidden(hidden: boolean) {
-        this.itemsContainerHidden = hidden;
+        if (!this.disabled) {
+            this.itemsContainerHidden = hidden;
+        }
     }
 
     setBackdrop(isAdding: boolean) {
-        const appRoot = document.getElementsByTagName('app-root');
+        const appRoot = $('app-root');
 
         if (isAdding) {
-            appRoot.item(0).innerHTML += `<div class="rk-backdrop"></div>`;
+            appRoot.append(`<div class="rk-backdrop"></div>`);
 
-            appRoot.item(0).addEventListener('click', () => {
-                this.setBackdrop(false);
-            });
+            $('.rk-backdrop').remove();
+
+            this.setItemsContainerHidden(false);
         } else {
-            const backDrop = document.getElementsByClassName('rk-backdrop');
-
-            // tslint:disable-next-line: prefer-for-of
-            for (let i = 0; i < backDrop.length; i++) {
-                const element = backDrop[i];
-
-                element.remove();
-            }
-
+            $('.rk-backdrop').remove();
         }
+    }
+
+    writeValue(obj: any): void {
+        this.selectedOption = obj;
+    }
+    registerOnChange(fn: () => void): void {
+        // throw new Error("Method not implemented.");
+    }
+    registerOnTouched(fn: () => void): void {
+        // throw new Error("Method not implemented.");
+    }
+    setDisabledState?(isDisabled: boolean): void {
+        // throw new Error("Method not implemented.");
     }
 
 }
