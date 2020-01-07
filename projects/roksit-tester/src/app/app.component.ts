12edce4f9b4fb0703450f1c6e02c45450f49ed84
Cihan Çelen <cihan.celen37@hotmail.com>
2020-01-07 14:37:59 +0300

    zz

--- projects/roksit-tester/src/app/app.component.ts
+++ projects/roksit-tester/src/app/app.component.ts
@@ -9,21 +9,22 @@ import { RkSelectModel } from 'projects/roksit-lib/src/lib/modules/rk-select/rk-
 export class AppComponent {
   title = 'roksit-tester';
 
-  options: RkSelectModel[] = [
-    { displayText: 'Text 1', value: 1 },
-    { displayText: 'Text 2', value: 1 },
-    { displayText: 'Text 3', value: 1 },
-    { displayText: 'Text 4', value: 1 },
-    { displayText: 'Text 6', value: 1 },
-    { displayText: 'Text 7', value: 1 },
-    { displayText: 'Text 8', value: 1 },
-    { displayText: 'Text 9', value: 1 },
-    { displayText: 'Text 10', value: 1 },
-    { displayText: 'Text 11', value: 1 },
-    { displayText: 'Text 12', value: 1 },
-    { displayText: 'Text 13', value: 1 },
-    { displayText: 'Text 14', value: 1 },
-    { displayText: 'Text 15', value: 1 },
-    { displayText: 'Text 16', value: 1 },
+  options: any[] = [
+    {
+      id: 'opt1',
+      text: 'Options 1'
+    },
+    {
+      id: 'opt2',
+      text: 'Options 2'
+    },
+    {
+      id: 'opt3',
+      text: 'Options 3'
+    },
+    {
+      id: 'opt4',
+      text: 'Options 4'
+    }
   ];
 }
