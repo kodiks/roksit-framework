50789f802c45f5e1054ad01706eae60b25859ace
mgunduzz <mzlmgndz@gmail.com>
2020-01-16 10:11:02 +0300

    add modules to public api

--- projects/roksit-tester/src/app/app.module.ts
+++ projects/roksit-tester/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
 // import { RoksitModule } from 'projects/roksit-lib/src/public-api';
-import { RoksitModule } from 'roksit-lib';
+import { RoksitModule  } from 'roksit-lib';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RkProgressBarModule } from 'roksit-lib/lib/modules/rk-progressbar/rk-progressbar.module';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     RoksitModule.forRoot(),
+    RkProgressBarModule,
     CommonModule,
     FormsModule
   ],
