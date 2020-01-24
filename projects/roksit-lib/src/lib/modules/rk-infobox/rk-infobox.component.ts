9bc620c19722bb5b7e8ee3bb99a15d3e00b28ea9
Cihan Çelen <cihan.celen37@hotmail.com>
2020-01-24 12:25:05 +0300

    rk-infobox bitti

--- projects/roksit-lib/src/lib/modules/rk-infobox/rk-infobox.component.ts
+++ projects/roksit-lib/src/lib/modules/rk-infobox/rk-infobox.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
     selector: 'rk-infobox',
@@ -9,27 +9,45 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 export class RkInfoBoxComponent implements OnInit {
     constructor() { }
 
+    // tslint:disable-next-line: no-input-rename
     @Input('active') isActive = false;
+    // tslint:disable-next-line: no-input-rename
     @Input('type') type = 1;
     @Input() title = '';
     @Input() color = '';
     @Input() valueOne = 0;
     @Input() valueTwo = 0;
+    @Input() description = '';
+
+    @ViewChild('svgG', { static: true }) svgG: ElementRef;
+
+    guid;
+
+
+
+    // tslint:disable-next-line: no-input-rename
+    @Input() className;
 
     ngOnInit() {
-        this.statusChanged();
+        this.guid = this.getGuid(6);
+
+        this.svgG.nativeElement.setAttribute('stroke', `url(#linearGradient-${this.guid})`);
+    }
+
+    getGuid(len) {
+        const buf = [];
+        const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        const charlen = chars.length;
+        const length = len || 32;
+
+        for (let i = 0; i < length; i++) {
+            buf[i] = chars.charAt(Math.floor(Math.random() * charlen));
+        }
+
+        return buf.join('');
     }
 
     toggleActive() {
         this.isActive = !this.isActive;
-
-        this.statusChanged();
     }
-
-
-    statusChanged() {
-
-    }
-
-
 }
