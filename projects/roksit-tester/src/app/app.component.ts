5ae893b6c6e70ccd30474122dbe4cc245d2abfe5
mgunduzz <mzlmgndz@gmail.com>
2020-01-29 16:28:18 +0300

    rkTableUpdate

--- projects/roksit-tester/src/app/app.component.ts
+++ projects/roksit-tester/src/app/app.component.ts
@@ -68,7 +68,7 @@ export class AppComponent implements OnInit {
 
   tableConfig: RkTableConfigModel = {
     columns: [
-      { id: 0, name: 'time', displayText: 'Time',selected : true },
+      { id: 0, name: 'time', displayText: 'Time' },
       { id: 1, name: 'domain', displayText: 'Domain' },
       { id: 2, name: 'subdomain', displayText: 'Subdomain' },
       { id: 3, name: 'srcip', displayText: 'Src.Ip' },
@@ -362,6 +362,9 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.tableConfig.columns[0].selected = true;
+    }, 100);
     // this.prepareTimelineChart();
 
     // RkApexHelper.render('#line-chart', {
