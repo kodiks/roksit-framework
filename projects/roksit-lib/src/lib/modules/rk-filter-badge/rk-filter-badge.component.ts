276b76360bd00851eecfa2d9c1aa4dcaf3c92988
Cihan Çelen <cihan.celen37@hotmail.com>
2020-02-29 19:53:30 +0300

    rk-filter-badge revize edildi.

--- projects/roksit-lib/src/lib/modules/rk-filter-badge/rk-filter-badge.component.ts
+++ projects/roksit-lib/src/lib/modules/rk-filter-badge/rk-filter-badge.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
+export interface RkFilterOutput {
+    name: string;
+    values: string[];
+    equal: boolean;
+
+    index?: number;
+}
+
 @Component({
     selector: 'rk-filter-badge',
     templateUrl: 'rk-filter-badge.component.html'
@@ -9,17 +17,27 @@ export class RkFilterBadgeComponent implements OnInit {
     constructor() { }
 
     @Input('name') filterName = '';
-    @Input('values') filterValues = '';
+    @Input('values') filterValues = [];
     @Input('equal') isEqual = false;
+
+    @Input('index') _index = -1;
 
     @Output('deleted') onDelete = new EventEmitter();
     @Output('edited') onEdit = new EventEmitter();
+    @Output() onChange = new EventEmitter();
 
 
     ngOnInit() { }
 
     toggleEqual() {
         this.isEqual = !this.isEqual;
+
+        this.onChange.emit({
+            name: this.filterName,
+            values: this.filterValues,
+            equal: this.isEqual,
+            index: this._index
+        } as RkFilterOutput);
     }
 
     onEditClick() {
