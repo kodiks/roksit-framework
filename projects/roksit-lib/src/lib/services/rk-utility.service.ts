1bf8921242fb727fd2cf146149b99bac3519e9b9
Cihan Çelen <cihan.celen37@hotmail.com>
2020-03-30 16:37:16 +0300

    fixess...

--- projects/roksit-lib/src/lib/services/rk-utility.service.ts
+++ projects/roksit-lib/src/lib/services/rk-utility.service.ts
@@ -4,12 +4,8 @@ import { Injectable } from '@angular/core';
     providedIn: 'root'
 })
 export class RkUtilityService {
-    themeVars: Array<{ key, lightVal, darkVal }> = [
-        { key: 'cardBgColor', lightVal: '#f0f3f8', darkVal: '#262626' },
-        { key: 'titleColor', lightVal: '#445165', darkVal: '#f5f5f5' },
-        { key: 'bodyColor', lightVal: '#ffffff', darkVal: '#000000' },
-        { key: 'rkTitlecolor', lightVal: '#445165', darkVal: '#ffffff' },
-    ];
+
+    themeVars: Array<{ key, lightVal, darkVal }> = [];
 
     changeTheme(isDark: boolean = false) {
         this.themeVars.forEach(varItem => {
